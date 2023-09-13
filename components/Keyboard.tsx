a844088ab7b027be9439be3fbd08aa7dbe9bd23b
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-12 22:08:15 -0300

    Adjust z-indexes

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { cn } from "@/lib/styling";
 
 interface KeyProps {
+  className?: string;
   type: "accidental" | "natural";
   note:
     | "C"
@@ -18,22 +19,23 @@ interface KeyProps {
     | "B";
 }
 
-export const Key: React.FC<KeyProps> = ({ type, note }) => {
+export const Key: React.FC<KeyProps> = ({ className, type, note }) => {
   return (
     <div
       className={cn(
-        "relative float-left ml-[2px] flex items-end justify-center rounded-b-sm border-neutral-900 pb-1 text-opacity-0 first:ml-0",
+        "relative float-left ml-[2px] flex cursor-pointer items-end justify-center rounded-b-sm border-neutral-900 pb-1 text-opacity-0 first:ml-0",
 
         type === "accidental" &&
-          "-left-[11px] z-10 -mr-[22px] h-[94px] w-[20px] bg-black text-white",
+          "-left-[11px] z-30 -mr-[22px] h-[94px] w-[20px] bg-black text-white",
 
-        type === "natural" && "h-[150px] w-[33px] bg-[#F4F1F4] text-black",
+        type === "natural" && "z-20 h-[150px] w-[33px] bg-[#F4F1F4] text-black",
+
+        className,
       )}
-    >
-      {note}
-    </div>
+    ></div>
   );
 };
+
 interface KeyboardProps {
   children?: React.ReactNode;
 }
@@ -43,7 +45,7 @@ export const Keyboard: React.FC<KeyboardProps> = ({ children }) => {
       className={cn(
         "relative z-20 flex h-full justify-center rounded bg-neutral-900",
         "before:content-[' '] before:absolute before:left-0 before:right-0 before:z-20 before:h-[150px] before:w-full before:rounded-[4px] before:shadow-inner",
-        "after:content-[' '] after:absolute after:left-0 after:right-0 after:z-20 after:h-[150px] after:w-full after:rounded-[4px] after:shadow-xl",
+        "after:content-[' '] after:absolute after:left-0 after:right-0 after:-z-20 after:h-[150px] after:w-full after:rounded-[4px] after:shadow-xl",
       )}
     >
       {children}
