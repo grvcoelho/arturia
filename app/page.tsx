fc4b249e27d56f2d1c0b8ceffa3cc62418ecf387
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-12 22:08:15 -0300

    Improve scales

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   return (
     <main className="container mx-auto flex min-h-screen flex-col items-center justify-center space-y-20">
       <div className="flex w-full items-center justify-center">
-        <Arturia className="scale-[0.6] sm:scale-100" />
+        <Arturia className="scale-[0.75] sm:scale-100 md:scale-110 lg:scale-125" />
       </div>
     </main>
   );
