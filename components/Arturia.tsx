c49a3e7cdbb45a03a34c8c8698f68e2bb69ae2ff
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-12 22:08:15 -0300

    Add pads

--- components/Arturia.tsx
+++ components/Arturia.tsx
@@ -1,7 +1,70 @@
 import React from "react";
 import { cn } from "@/lib/styling";
 
-const Key = ({ type, note }) => {
+/**
+ * text-red-500 ring-red-500
+ * text-orange-500 ring-orange-500
+ * text-amber-500 ring-amber-500
+ * text-yellow-500 ring-yellow-500
+ * text-lime-500 ring-lime-500
+ * text-green-500 ring-green-500
+ * text-emerald-500 ring-emerald-500
+ * text-teal-500 ring-teal-500
+ * text-cyan-500 ring-cyan-500
+ */
+type PadColor =
+  | "red"
+  | "orange"
+  | "amber"
+  | "yellow"
+  | "lime"
+  | "green"
+  | "emerald"
+  | "teal";
+
+interface PadProps {
+  color: PadColor;
+  children?: React.ReactNode;
+}
+
+const Pad: React.FC<PadProps> = ({ color, children }) => (
+  <div className=" flex h-[42px] w-[42px] items-center justify-center rounded-[6px] bg-black">
+    <div
+      className={cn(
+        "flex h-[38.5px] w-[38.5px] flex-col justify-end rounded-[4px] bg-black ring-[1.5px] ring-inset",
+        `ring-${color}-500`,
+      )}
+    >
+      <span
+        className={cn(
+          "mb-[3px] text-center font-mono text-[4px]",
+          `text-${color}-500`,
+        )}
+      >
+        {children}
+      </span>
+    </div>
+  </div>
+);
+
+interface KeyProps {
+  type: "accidental" | "natural";
+  note:
+    | "C"
+    | "C#"
+    | "D"
+    | "D#"
+    | "E"
+    | "F"
+    | "F#"
+    | "G"
+    | "G#"
+    | "A"
+    | "A#"
+    | "B";
+}
+
+const Key: React.FC<KeyProps> = ({ type, note }) => {
   return (
     <div
       className={cn(
@@ -18,7 +81,7 @@ const Key = ({ type, note }) => {
   );
 };
 
-const Keyboard = () => {
+const Keyboard: React.FC = () => {
   return (
     <div className="relative flex h-full justify-center rounded bg-neutral-900">
       <Key note="C" type="natural" />
@@ -50,7 +113,12 @@ const Keyboard = () => {
   );
 };
 
-function Arturia(props) {
+interface ArturiaProps {
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const Arturia: React.FC<ArturiaProps> = (props) => {
   const { className, style } = props;
   return (
     <div
@@ -77,7 +145,16 @@ function Arturia(props) {
               faders
             </div>
           </div>
-          <div className="flex-1 bg-green-100">pads</div>
+          <div className="ml-[10px] flex gap-[12.5px]">
+            <Pad color="red">Arp</Pad>
+            <Pad color="orange">Pad</Pad>
+            <Pad color="amber">Prog</Pad>
+            <Pad color="yellow">Loop</Pad>
+            <Pad color="lime">Stop</Pad>
+            <Pad color="green">Play</Pad>
+            <Pad color="emerald">Rec</Pad>
+            <Pad color="teal">Tap</Pad>
+          </div>
         </div>
       </div>
 
@@ -86,6 +163,6 @@ function Arturia(props) {
       </div>
     </div>
   );
-}
+};
 
 export default Arturia;
