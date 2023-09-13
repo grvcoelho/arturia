ae764325c0551e1bee5a6a21dc4c9deee53c541e
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-12 22:08:15 -0300

    Move Keyboard to components folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,5 @@
 import { cn } from "@/lib/styling";
-
-const Key = ({ children, type }) => {
-  console.log({ children, type });
-  return (
-    <div
-      className={cn(
-        "last:borde relative float-left flex items-end justify-center rounded-b-md border-2 border-neutral-900 pb-1",
-
-        type === "accidental" &&
-          "-left-4 z-10 -mr-8 h-24 w-8 bg-black  text-white",
-
-        type === "natural" &&
-          " h-44 w-10 border-r-0 bg-white text-black last:border-r-2",
-      )}
-    >
-      {children}
-    </div>
-  );
-};
-
-const Keyboard = () => {
-  return (
-    <div className="rounded-lg border-2 border-x-neutral-800 border-b-neutral-900 border-t-neutral-600 bg-neutral-700 p-3 shadow-lg">
-      <div className="relative flex rounded bg-neutral-900 p-1">
-        <Key type="natural">C</Key>
-        <Key type="accidental">C#</Key>
-        <Key type="natural">D</Key>
-        <Key type="accidental">D#</Key>
-        <Key type="natural">E</Key>
-        <Key type="natural">F</Key>
-        <Key type="accidental">F#</Key>
-        <Key type="natural">G</Key>
-        <Key type="accidental">G#</Key>
-        <Key type="natural">A</Key>
-        <Key type="accidental">A#</Key>
-        <Key type="natural">B</Key>
-        <Key type="natural">C</Key>
-      </div>
-    </div>
-  );
-};
+import Keyboard from "@/components/Keyboard";
 
 export default function Home() {
   return (
---
+++ components/Keyboard.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { cn } from "@/lib/styling";
+
+const Key = ({ type, note }) => {
+  return (
+    <div
+      className={cn(
+        "last:borde relative float-left flex items-end justify-center rounded-b-md border-2 border-neutral-900 pb-1",
+
+        type === "accidental" &&
+          "-left-4 z-10 -mr-8 h-24 w-8 bg-black  text-white",
+
+        type === "natural" &&
+          " h-44 w-10 border-r-0 bg-white text-black last:border-r-2",
+      )}
+    >
+      {note}
+    </div>
+  );
+};
+
+const Keyboard = () => {
+  return (
+    <div className="rounded-lg border-2 border-x-neutral-800 border-b-neutral-900 border-t-neutral-600 bg-neutral-700 p-3 shadow-lg">
+      <div className="relative flex rounded bg-neutral-900 p-1">
+        <Key note="C" type="natural" />
+        <Key note="C#" type="accidental" />
+        <Key note="D" type="natural" />
+        <Key note="D#" type="accidental" />
+        <Key note="E" type="natural" />
+        <Key note="F" type="natural" />
+        <Key note="F#" type="accidental" />
+        <Key note="G" type="natural" />
+        <Key note="G#" type="accidental" />
+        <Key note="A" type="natural" />
+        <Key note="A#" type="accidental" />
+        <Key note="B" type="natural" />
+        <Key note="C" type="natural" />
+      </div>
+    </div>
+  );
+};
+
+export default Keyboard;
