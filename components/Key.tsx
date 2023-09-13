8976a591697381ca8cb06863cf494ed54d69dc41
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-12 22:08:15 -0300

    Improve keyboard shadows

--- components/Key.tsx
+++ components/Key.tsx
@@ -36,11 +36,12 @@ export const Key: React.FC<KeyProps> = ({
         active && "active scale-[.99]",
 
         isAccidental &&
-          "-left-[11px] z-30 -mr-[22px] h-[94px] w-[20px] bg-black text-white",
+          "accidental -left-[11px] z-30 -mr-[22px] h-[94px] w-[20px] bg-neutral-950",
 
-        !isAccidental &&
-          "natural" &&
-          "z-20 h-[150px] w-[33px] bg-[#F4F1F4] text-black",
+        isAccidental &&
+          "border-x-[3px] border-b-8 border-b-neutral-900 border-l-neutral-700 border-r-neutral-800",
+
+        !isAccidental && "natural z-20 h-[150px] w-[33px] bg-[#F4F1F4]",
 
         [1, 6].includes(chroma) && "left-[-16px]",
 
