32c3b785f908b621385957bdd1aa86c6f33d585c
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-12 22:08:15 -0300

    Refactor Key component

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -1,9 +1,85 @@
-"use client";
-
-import { Soundfont } from "smplr";
 import React, { useState, useEffect } from "react";
+import TonalNote from "@tonaljs/note";
+import { Soundfont } from "smplr";
 import { cn } from "@/lib/styling";
-import { Key } from "./Key";
+
+type Note = {
+  midi: number;
+  oct: number;
+};
+
+const isMidiAccidental = (midi: number) => [1, 3, 6, 8, 10].includes(midi % 12);
+
+const notesFromOct = (oct: number): Array<Note> => {
+  // C1 starts at midi note 24
+  // C4 starts at midi note 60
+  const midiBase = oct * 12 + 12;
+
+  return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    .map((n) => n + midiBase)
+    .map((midi) => ({
+      midi,
+      oct,
+    }));
+};
+
+interface KeyProps {
+  className?: string;
+  note: Note;
+  onPress?: (note: Note) => void;
+  onRelease?: (note: Note) => void;
+}
+
+export const Key: React.FC<KeyProps> = ({
+  className,
+  note,
+  onPress,
+  onRelease,
+}) => {
+  const [active, setActive] = React.useState(false);
+  const chroma = TonalNote.get(TonalNote.fromMidi(note.midi)).chroma || -1;
+  const isAccidental = isMidiAccidental(note.midi);
+
+  const handlePressed = () => {
+    setActive(true);
+    onPress && onPress(note);
+  };
+
+  const handleReleased = () => {
+    setActive(false);
+    onRelease && onRelease(note);
+  };
+
+  return (
+    <div
+      className={cn(
+        "key",
+
+        "relative float-left ml-[2px] flex cursor-pointer items-end justify-center rounded-b-[1px] border-neutral-900 pb-1 first:ml-0",
+
+        "origin-top transition-transform duration-75 ease-in",
+
+        active && "active scale-[.99]",
+
+        isAccidental &&
+          "-left-[11px] z-30 -mr-[22px] h-[94px] w-[20px] bg-black text-white",
+
+        !isAccidental &&
+          "natural" &&
+          "z-20 h-[150px] w-[33px] bg-[#F4F1F4] text-black",
+
+        [1, 6].includes(chroma) && "left-[-16px]",
+
+        [3, 10].includes(chroma) && "left-[-6px]",
+
+        className,
+      )}
+      onMouseDown={handlePressed}
+      onMouseLeave={handleReleased}
+      onMouseUp={handleReleased}
+    ></div>
+  );
+};
 
 interface KeyboardProps {
   children?: React.ReactNode;
@@ -11,6 +87,12 @@ interface KeyboardProps {
 
 export const Keyboard: React.FC<KeyboardProps> = ({ children }) => {
   const [instrument, setInstrument] = useState<Soundfont | null>(null);
+  const [oct, setOct] = useState(4);
+  const notes = [
+    ...notesFromOct(oct),
+    ...notesFromOct(oct + 1),
+    notesFromOct(oct + 2)[0],
+  ];
 
   useEffect(() => {
     const ac = new AudioContext();
@@ -22,21 +104,6 @@ export const Keyboard: React.FC<KeyboardProps> = ({ children }) => {
     setInstrument(instrument);
   }, []);
 
-  const playNote = (note) => {
-    instrument?.start({
-      note,
-      duration: 200,
-    });
-
-    if (!instrument) return;
-    note.time = (note.time ?? 0) + instrument.context.currentTime;
-    instrument.start(note);
-  };
-
-  const stopNote = (midi) => {
-    instrument?.stop({ stopId: midi });
-  };
-
   return (
     <div
       className={cn(
@@ -46,181 +113,14 @@ export const Keyboard: React.FC<KeyboardProps> = ({ children }) => {
         "after:content-[' '] after:absolute after:left-0 after:right-0 after:-z-20 after:h-[166px] after:w-full after:rounded-[0px]",
       )}
     >
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="C"
-        octave={4}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="C#"
-        octave={4}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="D"
-        octave={4}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="D#"
-        octave={4}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="E"
-        octave={4}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="F"
-        octave={4}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="F#"
-        octave={4}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="G"
-        octave={4}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="G#"
-        octave={4}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="A"
-        octave={4}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="A#"
-        octave={4}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="B"
-        octave={4}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="C"
-        octave={5}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="C#"
-        octave={5}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="D"
-        octave={5}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="D#"
-        octave={5}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="E"
-        octave={5}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="F"
-        octave={5}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="F#"
-        octave={5}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="G"
-        octave={5}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="G#"
-        octave={5}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="A"
-        octave={5}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="A#"
-        octave={5}
-        type="accidental"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="B"
-        octave={5}
-        type="natural"
-      />
-      <Key
-        onPress={playNote}
-        onRelease={stopNote}
-        note="C"
-        octave={6}
-        type="natural"
-      />
+      {notes.map((note) => (
+        <Key
+          key={note.midi}
+          note={note}
+          onPress={() => {}}
+          onRelease={() => {}}
+        />
+      ))}
     </div>
   );
 };
