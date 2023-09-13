522d2d6d7e50ffdebec607fe5921b9c2045a2115
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-12 22:08:15 -0300

    Refactor pads

--- components/Pad.tsx
+++ components/Pad.tsx
@@ -1,17 +1,6 @@
 import React from "react";
 import { cn } from "@/lib/styling";
 
-/**
- * text-red-500 ring-red-500
- * text-orange-500 ring-orange-500
- * text-amber-500 ring-amber-500
- * text-yellow-500 ring-yellow-500
- * text-lime-500 ring-lime-500
- * text-green-500 ring-green-500
- * text-emerald-500 ring-emerald-500
- * text-teal-500 ring-teal-500
- * text-cyan-500 ring-cyan-500
- */
 type PadColor =
   | "red"
   | "orange"
@@ -22,27 +11,36 @@ type PadColor =
   | "emerald"
   | "teal";
 
+const variants = {
+  red: { text: "text-red-500", ring: "ring-red-500" },
+  orange: { text: "text-orange-500", ring: "ring-orange-500" },
+  amber: { text: "text-amber-500", ring: "ring-amber-500" },
+  yellow: { text: "text-yellow-500", ring: "ring-yellow-500" },
+  lime: { text: "text-lime-500", ring: "ring-lime-500" },
+  green: { text: "text-green-500", ring: "ring-green-500" },
+  emerald: { text: "text-emerald-500", ring: "ring-emerald-500" },
+  teal: { text: "text-teal-500", ring: "ring-teal-500" },
+};
+
 interface PadProps {
   color: PadColor;
   children?: React.ReactNode;
 }
 
-export const Pad: React.FC<PadProps> = ({ color, children }) => (
-  <div className="flex h-[42px] w-[42px] cursor-pointer items-center justify-center rounded-[6px] bg-black">
-    <div
-      className={cn(
-        "flex h-[38.5px] w-[38.5px] flex-col justify-end rounded-[4px] bg-black ring-[1.5px] ring-inset",
-        `ring-${color}-500`,
-      )}
-    >
-      <span
+export const Pad: React.FC<PadProps> = ({ color, children }) => {
+  const { text, ring } = variants[color];
+  return (
+    <div className="flex h-[42px] w-[42px] cursor-pointer items-center justify-center rounded-[6px] bg-black">
+      <div
         className={cn(
-          "mb-[3px] text-center font-mono text-[4px]",
-          `text-${color}-500`,
+          "flex h-[38.5px] w-[38.5px] flex-col justify-end rounded-[4px] bg-black ring-[1.5px] ring-inset",
+          ring,
         )}
       >
-        {children}
-      </span>
+        <span className={cn("mb-[3px] text-center font-mono text-[4px]", text)}>
+          {children}
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
